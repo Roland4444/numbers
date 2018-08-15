77831a9f207eca4db13bb32c686a626ad5b5ea0a
Roland4444 <turbopollux@rambler.ru>
2018-08-15 15:17:24 +0000

    need to add gramatic

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-dictionary = {1: "тысяч", 2: "миллион", 3: "миллиард"}
+dictionary = {0: "", 1: "", 2: "тысяч", 3: "миллион", 4: "миллиард", 5: "триллион"}
 
 dicthundreds = {"1": "сто", "2": "двести", "3": "триста",
     "4": "четыреста",
@@ -21,34 +21,64 @@ dictunits = {"1": "один", "2": "два", "3": "три", "4": "четыре",
     "12": "двенадцать",
     "13": "тринадцать", "14": "четырнадцать", "15": "пятьнадцать",
     "16": "шестьнадцать",
-    "17": "семьнадцать", "18": "восемнадцать", "19": "девятнадцать"}
+    "17": "семнадцать", "18": "восемнадцать", "19": "девятнадцать"}
 
 
 def subnumb(input):
     result = ""
+    if input == "000":
+        return result
     if input[0] != "0":
-        result += dicthundreds[input[0]]
+        result += " " + dicthundreds[input[0]]
     if (input[1] != "0" and input[1] != "1"):
-        result += dictdecimals[input[1]]
-        result += dictunits[input[2]]
+        result += " " + dictdecimals[input[1]]
+        result += " " + dictunits[input[2]]
         return result
     suffix = input[1] + input[2]
-    result += dictunits[suffix]
+    result += " " + dictunits[suffix]
     return result
 
 
-def numbers(input):
-    digest = len(input)
-    counter = len(input) // 3
+def numbers(input_):
+    input  = prepared(input_)
+    groups = len(input) // 3
+    print("groups", groups)
+
+    print(dictionary[groups])
     result = ""
-    result += dictionary[counter]
+
+    iter=0
+    while (groups!=0):
+
+        prefix = input[iter:iter+3]
+        print("prefix=", prefix)
+        iter += 3
+        if subnumb(prefix) == "":
+            continue
+        print("grops", groups)
+        print("dictionary[groups]", dictionary[groups])
+        result += subnumb(prefix) + dictionary[groups]
+
+        groups -= 1
+
+    print(result)
+
     return result
 
-print(subnumb("123"))
-print(subnumb("466"))
-print(subnumb("103"))
-print(subnumb("999"))
-print(numbers("1000000000"))
-print(numbers("1000000"))
-print(numbers("1156"))
-assert numbers("156") == "сто пятьдесят шесть"
+
+def prepared(input):
+    rest= 3 - (len(input) % 3)
+    if (rest == 3):
+        return input
+    result = ""
+
+    while (rest != 0):
+        rest -= 1
+        result += "0"
+
+    result += input
+
+    print("result", result)
+    return result
+
+numbers("3454334")
